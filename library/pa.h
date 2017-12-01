059458d95de2647bbbbe6b490b861e4f895996c3
Nikola Timkova <nik.tiimm@gmail.com>
2017-12-01 11:56:04 +0100

    header

---
+++ library/pa.h
@@ -0,0 +1,23 @@
+#ifndef UNTITLED2_PA_H
+#define UNTITLED2_PA_H
+
+#include <stdbool.h>
+
+int expeRetype(int typeA, int typeB);
+unsigned int get_table_index(int flag);
+bool is_handle(PAList *l);
+
+typedef struct items{
+    int id;
+    bool is_terminal;
+    bool E_handle;
+    struct items *next;
+    struct items *prev;
+}PAItem;
+
+typedef struct {
+    PAItem *first;
+    PAItem *last;
+    PAItem *lastTerminal;
+}PAList;
+#endif //UNTITLED2_PA_H
