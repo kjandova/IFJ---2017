22619b9cff0873dd7b7f1eb77a1ba0d4a8004388
Nikola Timkova <nik.tiimm@gmail.com>
2017-12-05 04:36:51 +0100

    Add files via upload

--- library/pa.h
+++ library/pa.h
@@ -1,3 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+//  @Project            IFJ 2017
+//
+//  @Authors
+//  Kristýna Jandová    xjando04
+//  Vilém Faigel        xfaige00
+//  Nikola Timková      xtimko01
+//  Bc. Václav Doležal  xdolez76
+//
+//  @File               pa.h
+//  @Description
+//
+//
+///////////////////////////////////////////////////////////////////////////////////
+
 #ifndef IFJ_PA_H
 #define IFJ_PA_H
 
@@ -24,11 +40,11 @@ typedef struct PAList{
 }PAList;
 
 
-void prec_anal();
+int prec_anal();
+void delete_list(PAList *a);
 void init_list(PAList *l);
 PAItem * last_terminal(PAList *l);
 
-bool is_expression(PAList *l);
 void insert_item(PAList *l, int flag, string name);
 
 
